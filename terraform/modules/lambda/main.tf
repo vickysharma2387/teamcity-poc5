9cac5ef95814931f3029bc4fa3ef6233e94ca256
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-10 11:41:03 +0530

    Updated lambda/main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -14,8 +14,8 @@ resource "aws_lambda_function" "hello_world" {
 resource "aws_lambda_permission" "allow_s3_invoke" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.arn
-principal = "s3.amazonaws.com"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "s3.amazonaws.com"
   source_arn    = var.s3_bucket_arn
 }
  
