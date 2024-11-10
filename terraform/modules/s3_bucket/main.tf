6fecd44025808333eb9045e945c8697018e7a434
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-10 10:08:26 +0530

    Updated deploy.yml and S3

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -1,6 +1,12 @@
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = var.bucket_name
-  acl    = "private"
+}
+ 
+# Upload the Lambda code zip file to the S3 bucket
+resource "aws_s3_object" "lambda_zip" {
+bucket = aws_s3_bucket.lambda_bucket.id
+key = "lambda.zip"
+source = "lambda.zip"
 }
  
 output "bucket_name" {
