a34a805d09cec1818247bec0e7ea902a9edf172c
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-10 10:55:28 +0530

    Updated main.tf and s3/variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "s3" {
   source      = "./modules/s3"
   bucket_name = var.bucket_name
-  lambda_function_arn = module.lambda_function.lambda_function_name
+  lambda_function_arn = module.lambda.lambda_function_name
 }
 
 module "iam_role" {
@@ -19,6 +19,6 @@ module "lambda" {
   lambda_function_name  = var.lambda_function_name
   lambda_role_arn       = module.iam_role.lambda_role_arn
   bucket_name           = module.s3.bucket_name
-  s3_bucket_arn             = "arn:aws:s3:::${module.s3.bucket_name}"
+  s3_bucket_arn         = "arn:aws:s3:::${module.s3.bucket_name}"
   environment_variables = var.lambda_environment_variables
 }
--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "lambda_function_arn" {
+  description = "ARN of the Lambda"
+  type        = string
+}
