c68e242ffe4d7b86cd4309f493c6354f4cce3a0e
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-10 10:38:42 +0530

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,10 +6,6 @@ variable "lambda_function_name" {
   type = string
 }
  
-variable "lambda_role_arn" {
-  type = string
-}
- 
 variable "lambda_environment_variables" {
   type        = map(string)
   default     = {}
