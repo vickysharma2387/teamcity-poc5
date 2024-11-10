d636e42b79bd4421e98c029d1048c6adb687a6ea
Vicky Sharma <vicky_sharma@persistent.com>
2024-11-10 20:01:35 +0530

    Updated s3 amd main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -33,6 +33,6 @@ output "bucket_name" {
 value = aws_s3_bucket.lambda_bucket.id
 }
 
-output "lambda_zip_key" {
-value = aws_s3_object.lambda_zip.key
+output "lambda_zip_version_id" {
+value = aws_s3_object.lambda_zip.version_id
 }
